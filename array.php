ff8c7521231d4b3f484ee747606d6995341b294b
moorthy21sparkout <163838347+moorthy21sparkout@users.noreply.github.com>
2024-04-03 11:05:42 +0530

    array in php

---
+++ array.php
@@ -0,0 +1,27 @@
+<?php
+$car= array("volvo","bmw","tata");
+echo $car[0];  # count the cars;
+
+?>
+<html>
+    <body>
+        
+    
+<pre>
+    <?php
+    $a=array("one","two","three");
+    $a[1]="one";
+     echo var_dump($a);
+
+
+
+?>
+<?php
+$number=array("one","two","three");
+foreach($number as $num);
+var_dump ($num);
+
+?>
+</pre>
+</body>
+</html>
