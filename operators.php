1fd1006107d93430b4de09a3c4b137f69f84555e
moorthy21sparkout <163838347+moorthy21sparkout@users.noreply.github.com>
2024-04-03 11:05:06 +0530

    operators

---
+++ operators.php
@@ -0,0 +1,69 @@
+<?php
+$a=5;
+$b=10;
+echo($a+$b);echo"<br>";
+echo($a-$b);echo"<br>";
+echo($a*$b);echo"<br>";
+echo($a/$b);echo"<br>";
+echo($a%$b);echo"<br>";
+echo "<hr>";
+
+?> 
+<?php
+
+$b="assignment operator";
+echo $b;echo"<br>";
+
+$a=54;
+echo $a;echo"<br>";
+$a=10;
+$a+=30;
+echo $a;echo"<br>";
+$a=12;
+$a-=10;
+echo $a;
+echo "<hr>";
+echo "<br>";
+
+
+
+
+ ?>
+ <?php
+ echo "increament and decreament";
+ echo "<br>";
+ $a=10;
+ echo $a++;
+ echo $a;
+ echo "<br>";
+
+
+ ?>
+ <?php
+ $year=2025;
+ if(($year%400==0)||($year%100!=0)&&($year%4==0))
+ {
+    echo "year is leap";
+ }
+ else{
+    echo "year is not leap";
+ }
+ 
+ 
+ ?>
+ <?php
+ $x="moor";
+ $y="thy";
+ echo $x . $y;
+ echo "<br>";
+$x .= $y;
+echo $x;
+?>
+
+ 
+ 
+ 
+ 
+ 
+ 
+
