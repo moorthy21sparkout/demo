bd13ed7cecef52a988c0e3f4be796b4d60e56d24
moorthy21sparkout <163838347+moorthy21sparkout@users.noreply.github.com>
2024-04-03 11:04:33 +0530

    constants

---
+++ constant.php
@@ -0,0 +1,31 @@
+<?php
+define("bikes",
+[
+   "honda",
+    "hero",
+    "royal"
+]);
+echo bikes[0];
+echo var_dump(bikes);
+?>
+<?php
+define("hello","<br>every one!");
+function display()
+{
+    echo hello;
+   
+}
+display();
+?>
+<?php
+$a=15;
+$b=25;
+$c=$a>$b ? "true." : "false.";
+echo $c;
+?>
+
+ 
+ 
+
+
+
