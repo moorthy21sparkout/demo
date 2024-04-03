098b5a472d5f3c615bd0fec466e57ed73b104b7b
moorthy21sparkout <163838347+moorthy21sparkout@users.noreply.github.com>
2024-04-03 18:09:43 +0530

    additional file in form

---
+++ new.php
@@ -0,0 +1,23 @@
+<html>
+    <body>
+        
+welcome <?php echo $_POST["name"]; ?><br>
+           your email id address is :<?php echo $_POST["email"]; ?>
+
+           </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           </html>
