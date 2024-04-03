7f446d16198df83479c880bf1804cee114444bbb
moorthy21sparkout <163838347+moorthy21sparkout@users.noreply.github.com>
2024-04-03 14:31:41 +0530

    array (associative  array,multidimensional array)

--- array.php
+++ array.php
@@ -1,27 +1,28 @@
+<?php    #associative array.
+$cars=array("brand=>tata<br>","modal=>swift<br>","year=>2004<br>");
+var_dump($cars);
+?>  
 <?php
-$car= array("volvo","bmw","tata");
-echo $car[0];  # count the cars;
+$local=array(
+        array(1,"ram",43898),
+        array(2,"faiz",6543),
+        array(3,"jose",8765)
+
+);
+for($row=0;$row<3;$row++)
+{
+    for($col=0;$col<3;$col++)
+    {
+     echo $local[$row][$col]." ";   
+    }
+    echo "<br>";
+  
+}
+
+
+
+
+
+
 
 ?>
-<html>
-    <body>
-        
-    
-<pre>
-    <?php
-    $a=array("one","two","three");
-    $a[1]="one";
-     echo var_dump($a);
-
-
-
-?>
-<?php
-$number=array("one","two","three");
-foreach($number as $num);
-var_dump ($num);
-
-?>
-</pre>
-</body>
-</html>
